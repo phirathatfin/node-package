80dcea2bd9c88c34eb867f69af4c60df3e493b51
phgoff <phirathat.ji@gmail.com>
2023-07-20 14:18:13 +0700

    add changeset on 5,6

---
+++ .changeset/hungry-grapes-scream.md
@@ -0,0 +1,5 @@
+---
+"test-changeset-version": patch
+---
+
+Add 5,6 in single changeset
--- index.js
+++ index.js
@@ -5,3 +5,6 @@ console.log("2 * 2 = " + 2 * 2);
 
 console.log("3 + 3 = " + (3 + 3));
 console.log("4 * 4 = " + 4 * 4);
+
+console.log("5 + 5 = " + (5 + 5));
+console.log("6 * 6 = " + 6 * 6);
